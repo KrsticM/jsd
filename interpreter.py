4af136eed7a679b79a846753f61c2dc7f4e61d9b
jaseyrae9 <jelena.surlan9@uns.ac.rs>
2020-06-14 12:49:03 +0200

    Generating text, header and image
    Jinja templates for text, header and image.

--- interpreter.py
+++ interpreter.py
@@ -1,9 +1,15 @@
 from textx import metamodel_from_file
+from os.path import join, dirname
 import psycopg2
+import jinja2
+import datetime
 
 metamodel = metamodel_from_file('grammar.tx')
 model = metamodel.model_from_file('document.docv')
 con = psycopg2.connect(database="jsd", user="postgres", password="postgres", host="127.0.0.1", port="5432")
+
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(join(dirname(__file__), 'templates')), trim_blocks=True, lstrip_blocks=True)
+now = datetime.datetime.now().strftime("%a, %b %d, %Y %X")
 
 all_data = {}
 all_data_colnames = {}
@@ -11,8 +17,6 @@ all_data_colnames = {}
 
 
 def parse_data(data):
-    print(data.name)
-    print(data.data_value)
     cur = con.cursor()
     cur.execute(data.data_value)
     rows = cur.fetchall()
@@ -29,17 +33,35 @@ def parse_data(data):
     #     print(row[4])
     #     print(row[5] + "\n")
 
-    
-
-# Main loop
-for document_statement in model.document_statement:
-    print(document_statement)
-    print(document_statement.data)
-    if(document_statement.data != None):
-        parse_data(document_statement.data)
+def parse_text(data):    
+    data_type = data.__class__.__name__ 
+    if(data_type == 'Text'):
+        template = jinja_env.get_template('text.j2')
+        output_folder.write(template.render(text=data.text, datetime=now))
+    elif(data_type == 'Header'):
+        template = jinja_env.get_template('header.j2')
+        output_folder.write(template.render(data=data, datetime=now))
     else:
-        print("Exception")
+        print('propao u else')
 
-con.close()
-print(all_data)
-print(all_data_colnames)
+def parse_image(data):
+    template = jinja_env.get_template('image.j2')
+    output_folder.write(template.render(image_source=join('../', data.image_source), datetime=now))
+
+# main fuction
+if __name__ == "__main__":
+    output_folder = open('generated/output.html', 'w')
+    for document_statement in model.document_statement:
+        if(document_statement.data != None):
+            parse_data(document_statement.data)
+        elif(document_statement.text != None):
+            parse_text(document_statement.text)
+        elif(document_statement.image != None):
+            parse_image(document_statement.image)
+        else:
+            print("Exception")
+
+    con.close()
+    output_folder.close()
+    print(all_data)
+    print(all_data_colnames)
